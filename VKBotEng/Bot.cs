11c6b741801071b864ca9506fea72f6489ba6c59
Daniil <39687893+EYE5@users.noreply.github.com>
2019-03-02 22:31:37 +0700

    Добавьте файлы проекта.

---
+++ VKBotEng/Bot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Enums.SafetyEnums;
+
+namespace VKBotEng
+{
+    class Bot
+    {
+        private VkApi VkData;
+
+
+         public Bot()
+        {
+            VkData = new VkApi();
+            VkData.Authorize(new ApiAuthParams(){AccessToken = "1aa508fa46aaa32670c3bd58863fc2d796b694a2b186a9c2866942523da8a984b82f9eac45f161e91c04e" });
+           
+        }
+
+
+
+    }
+}
---
+++ VKBotEng/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace VKBotEng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot VK = new Bot();
+            foreach (var mes in VK.GetMessages())
+            {
+                Console.WriteLine(mes);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
